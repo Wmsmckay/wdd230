657d2fba6f7a6f78859630a0a03fc3ae45e8f20b
McKay <wmsmckay@gmail.com>
2022-02-01 12:37:04 -0700

    added favicon

---
+++ chamber/js/script.js
@@ -0,0 +1,31 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+const lastUpdate = `Last Updated ${document.lastModified}`;
+const year = new Date().getFullYear();
+
+document.querySelector("#last-updated").textContent = lastUpdate;
+document.querySelector("#current-year").textContent = year;
+
+const today = new Date();
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+
+const weekNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+let wd = weekNames[today.getDay()]
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = monthNames[today.getMonth()];
+let yyyy = today.getFullYear();
+
+const currentDate = `${wd}, ${mm} ${dd}, ${yyyy}`;
+
+document.querySelector("#header-date").textContent = currentDate;
